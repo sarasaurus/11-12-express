49116f4f440b7d2771578ae2a5f3161846558534
sarasaurus <bixler.s.e@gmail.com>
2018-04-30 17:11:17 -0700

    scaffolded and tests passing

---
+++ src/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import startServer from './lib/server';
+
+
+startServer();
+
+// sooo i guess it doesn't matter what we do here, becauase the server is starting and stopping in server.test.js
---
+++ src/route/tree-router.js
@@ -0,0 +1,56 @@
+'use strict';
+
+import { Router } from 'express';
+import bodyParser from 'body-parser';
+import Tree from '../model/tree';
+import logger from '../lib/logger';
+
+const jsonParser = bodyParser.json();
+
+const noteRouter = new Router();
+// const noteRouter = module.exports = new Router(); // ES5
+
+noteRouter.post('/api/trees', jsonParser, (request, response) => {
+  logger.log(logger.INFO, 'POST - processing a request');
+  if (!request.body.title) {
+    logger.log(logger.INFO, 'Responding with a 400 error code');
+    return response.sendStatus(400);
+  }
+  return new Tree(request.body).save()
+    .then((note) => {
+      logger.log(logger.INFO, 'POST - responding with a 200 status code');
+      return response.json(note);
+    })
+    .catch((error) => {
+      logger.log(logger.ERROR, '__POST_ERROR__');
+      logger.log(logger.ERROR, error);
+      return response.sendStatus(500);
+    });
+});
+
+
+noteRouter.get('/api/trees/:id', (request, response) => {
+  logger.log(logger.INFO, 'GET - processing a request');
+
+  return Tree.findById(request.params.id)
+    .then((item) => { // Vinicio - note found OR note not found, but the id looks good
+      if (!item) {
+        logger.log(logger.INFO, 'GET - responding with a 404 status code - (!item)');
+        return response.sendStatus(404);
+      }
+      logger.log(logger.INFO, 'GET - responding with a 200 status code');
+      return response.json(item);
+    })
+    .catch((error) => { // Vinicio - mongodb error or parsing id error
+      if (error.message.toLowerCase().indexOf('cast to objectid failed') > -1) {
+        logger.log(logger.INFO, 'GET - responding with a 404 status code - objectId');
+        logger.log(logger.VERBOSE, `Could not parse the specific object id ${request.params.id}`);
+        return response.sendStatus(404);
+      }
+      logger.log(logger.ERROR, '__GET_ERROR__ Returning a 500 status code');
+      logger.log(logger.ERROR, error);
+      return response.sendStatus(500);
+    });
+});
+
+export default noteRouter;
